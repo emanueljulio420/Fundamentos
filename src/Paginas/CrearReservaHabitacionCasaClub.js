6bf4c6fd4be4088ec2cd09e01098dd3282495bcf
Luis Felipe Gomez Giraldo <100101763+PipeGom@users.noreply.github.com>
2022-10-31 16:15:15 -0500

    Add files via upload

---
+++ src/Paginas/CrearReservaHabitacionCasaClub.js
@@ -0,0 +1,17 @@
+import HeaderVolver from "../Componentes/Header_volver";
+//import Cotizacion from "../Componentes/Cotizacion";
+import HabitacionCasaClub from "../Componentes/CrearReservaHabitacionCasaClub";
+
+
+function CrearReservaHabitacionCasaClub() {
+    return (
+      <div className="index">
+          <HeaderVolver />
+          <HabitacionCasaClub/>
+      </div>
+    );
+  }
+  
+  export default CrearReservaHabitacionCasaClub;
+
+
