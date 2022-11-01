d5b8ba90d0675fea40479e8d012d73e9bbafcbb4
Luis Felipe Gomez Giraldo <100101763+PipeGom@users.noreply.github.com>
2022-10-31 19:09:33 -0500

    Add files via upload

---
+++ src/Paginas/ModificarArrendamiento.js
@@ -0,0 +1,15 @@
+import HeaderVolver from "../Componentes/Header_volver";
+//import Cotizacion from "../Componentes/Cotizacion";
+import ModificarArriendo from "../Componentes/ModificarArrendamientoVives";
+
+
+function ModificarReservaHabitacionCasaClub() {
+    return (
+      <div className="index">
+          <HeaderVolver />
+          <ModificarArriendo/>
+      </div>
+    );
+  }
+  
+  export default ModificarReservaHabitacionCasaClub;
