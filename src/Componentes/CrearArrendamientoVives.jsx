89c590f8e7a05100b5e7fe56aaaadcc0c0d55a0f
Emanuel <emanueljulio320@gmail.com>
2022-11-15 01:09:22 -0500

    Creo (parte 2)

--- src/Componentes/CrearArrendamientoVives.jsx
+++ src/Componentes/CrearArrendamientoVives.jsx
@@ -145,18 +145,65 @@ function CrearArrendamientoVives(){
               <input type="date" className="form-control" onChange={(ev) => setFechaI(ev.target.value)} id="fecha" required />
             </div>
             <div className="col-md-4">
-              <label for="inputEmail4" className="form-label">
-                Número de la habitación
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="nombreE"
-                onChange={(ev) => setHabitacion(ev.target.value)}
-                placeholder="Número de habitación"
-                required
-              />
-            </div>
+                <label html="disabledSelect" className="form-label">
+                  Habitación
+                </label>
+                <select id="montaje" className="form-select" onChange={(ev) => setHabitacion(ev.target.value)} required>
+                  <option>Seleccione una habitacion</option>
+                  <option value={101}>101</option>
+                  <option value={102}>102</option>
+                  <option value={103}>103</option>
+                  <option value={104}>104</option>
+                  <option value={201}>201</option>
+                  <option value={202}>202</option>
+                  <option value={203}>203</option>
+                  <option value={204}>204</option>
+                  <option value={301}>301</option>
+                  <option value={302}>302</option>
+                  <option value={303}>303</option>
+                  <option value={304}>304</option>
+                  <option value={401}>401</option>
+                  <option value={402}>402</option>
+                  <option value={403}>403</option>
+                  <option value={404}>404</option>
+                  <option value={501}>501</option>
+                  <option value={502}>502</option>
+                  <option value={503}>503</option>
+                  <option value={504}>504</option>
+                  <option value={601}>601</option>
+                  <option value={602}>602</option>
+                  <option value={603}>603</option>
+                  <option value={604}>604</option>
+                  <option value={701}>701</option>
+                  <option value={702}>702</option>
+                  <option value={703}>703</option>
+                  <option value={704}>704</option>
+                  <option value={801}>801</option>
+                  <option value={802}>802</option>
+                  <option value={803}>803</option>
+                  <option value={804}>804</option>
+                  <option value={901}>901</option>
+                  <option value={902}>902</option>
+                  <option value={903}>903</option>
+                  <option value={904}>904</option>
+                  <option value={1001}>1001</option>
+                  <option value={1002}>1002</option>
+                  <option value={1003}>1003</option>
+                  <option value={1004}>1004</option>
+                  <option value={1101}>1101</option>
+                  <option value={1102}>1102</option>
+                  <option value={1103}>1103</option>
+                  <option value={1104}>1104</option>
+                  <option value={1105}>1105</option>
+                  <option value={1106}>1106</option>
+                  <option value={1107}>1107</option>
+                  <option value={1108}>1108</option>
+                  <option value={1109}>1109</option>
+                  <option value={1110}>1110</option>
+                  <option value={1111}>1111</option>
+
+                </select>
+              </div>
 
             <h6>Información del Tutor (Solo para menores de edad):</h6>
 
